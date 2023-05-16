6449122ecffa4115415572c1e2e10c7e90405a61
Artem Popov <artfwo@gmail.com>
2023-05-16 23:25:49 +0200

    Set classifiers and description in setup.py

--- setup.py
+++ setup.py
@@ -35,9 +35,19 @@ setup(
     author_email="art@artfwo.net",
     url="https://github.com/artfwo/aalink",
     description="Async Python interface for Ableton Link",
-    long_description="",
+    long_description=open("README.rst").read(),
     ext_modules=[ext],
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
     python_requires=">=3.7",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Topic :: Artistic Software",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Networking",
+    ]
 )
